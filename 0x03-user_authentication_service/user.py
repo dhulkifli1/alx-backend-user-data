6c91690f3257bfbc7f970ec4b15676ac83736d13
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-08-17 16:01:45 +0300

    Declarative base

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,9 +2,13 @@
 '''Contains SQLALCHEMY model User'''
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class User():
+Base = declarative_base()
+
+
+class User(Base):
     '''SQLAlchemy model of User'''
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, nullable=False)
