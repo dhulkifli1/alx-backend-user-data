cdb7e6f17e018cd84a423beedeceb24a8fc396de
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-08-17 15:56:16 +0300

    Attempt

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Contains SQLALCHEMY model User'''
+
+from sqlalchemy import Column, Integer, String
+
+
+class User():
+    '''SQLAlchemy model of User'''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(1000), nullable=False)
+    hashed_password = Column(String(1000), nullable=False)
+    session_id = Column(String(1000))
+    reset_token = Column(String(1000))
